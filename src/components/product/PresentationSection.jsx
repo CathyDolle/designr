4249cabe296aac4d587d45497bdf9513f264f3fa
CathyDolle <cathy.dolle@live.fr>
2020-12-18 10:41:37 +0100

    online website

--- src/components/product/PresentationSection.jsx
+++ src/components/product/PresentationSection.jsx
@@ -3,13 +3,13 @@ import './presentationSection.scss'
 import { NavLink } from 'react-router-dom';
 import linkArrow from '../../assets/img/product/linkArrow.svg'
 
-function sectionRight({text, visual, title, to, link}) {
+function sectionRight({text, visual, title, to, link, href}) {
   return (
     <section className="presentationSection">
       <div className="textContainer">
         <h1>{title}</h1>
         <p>{text}</p>
-        { link && <NavLink to={to}>{link}<img src={linkArrow} alt="linkArrow"/></NavLink>}
+        { link && <NavLink to={to}>{link}<img src={linkArrow} alt="linkArrow"/></NavLink> && <a href={href} target="_blank" rel="noreferrer">{link}<img src={linkArrow} alt="linkArrow"/></a>}
       </div>
       <div className="visualContainer">
         <img src={visual} alt="visual"/>
